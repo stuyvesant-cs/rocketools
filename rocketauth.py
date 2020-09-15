07f132158a617fda9400ba331ef3587308bd776a
dw <dw@stuy.edu>
2020-09-15 01:14:53 +0000

    added api based authentication

--- rocketauth.py
+++ rocketauth.py
@@ -1,5 +1,8 @@
 TOKEN_FILE = 'token.txt'
 
+from requests import get, post
+import json
+import getpass
 
 #set tokens from file
 def get_tokens(tokenf = TOKEN_FILE):
@@ -11,5 +14,21 @@ def get_tokens(tokenf = TOKEN_FILE):
     return (auth, uid)
 
 
+def auth_login():
+    name = input("Enter username: ")
+    passw = getpass.getpass()
+    data = { 'user' : name, 'password': passw }
+    r = post('http://localhost:3000/api/v1/login', data = json.dumps(data))
+    data = r.json()['data']
+    return { 'X-User-Id' : data['userId'], 'X-Auth-Token' : data['authToken'] }
 
+def auth_logout( token ):
+    r = post('http://localhost:3000/api/v1/logout', headers = token)
+    print(r.text)
+    
 
+if __name__ == '__main__':
+    
+    token = auth_login()
+    print(token)
+    auth_logout(token) 
