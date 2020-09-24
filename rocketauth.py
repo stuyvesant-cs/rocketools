6c910cd46baad418c0ae226cec0c7864617c0a04
JonAlf <jonalf@gmail.com>
2020-09-24 09:05:05 -0400

    change auth to use token file in same way as login

--- rocketauth.py
+++ rocketauth.py
@@ -1,18 +1,16 @@
-TOKEN_FILE = 'token.txt'
-
 from requests import get, post
 import json
 import getpass
 
 #set tokens from file
-def get_tokens(tokenf = TOKEN_FILE):
+def get_tokens():
+    tokenf = input('token file: ')
     f = open(tokenf)
     lines = f.read().split('\n')
     f.close()
     auth = lines[0].split(',')[1]
     uid = lines[1].split(',')[1]
-    return (auth, uid)
-
+    return { 'X-User-Id' : uid, 'X-Auth-Token' : auth }
 
 def auth_login():
     name = input("Enter username: ")
@@ -28,7 +26,15 @@ def auth_logout( token ):
     
 
 if __name__ == '__main__':
-    
-    token = auth_login()
+    choice = """
+0: Authenticate with name/password
+1: Autheticate with token file
+(default 0): 
+"""
+    if choice == '1':
+        token = get_tokens()
+    else:
+        token = auth_login()
     print(token)
-    auth_logout(token) 
+    if choice != '1':
+        auth_logout(token) 
